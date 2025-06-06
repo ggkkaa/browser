411d20099205352f74b8e20a1a8cd91679880fd7
DcraftBg <czl_obshta@abv.bg>
2025-06-06 16:35:12 +0300

    Skip <!DOCTYPE html>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <raylib.h>
 #include <fileutils.h>
+#include <string.h>
 
 #define W_RATIO 16
 #define H_RATIO 9
@@ -11,7 +12,9 @@ int main(void) {
     // TODO: Unhardcode this sheizung
     const char* example_path = "examples/barebones.html";
     size_t content_size;
-    const char* content = read_entire_file(example_path, &content_size);
+    char* content_data = (char*)read_entire_file(example_path, &content_size);
+    char* content = content_data;
+    if(memcmp(content, "<!DOCTYPE html>", 15) == 0) content += 15;
     InitWindow(WIDTH, HEIGHT, "Bikeshed");
     while(!WindowShouldClose()) {
         BeginDrawing();
