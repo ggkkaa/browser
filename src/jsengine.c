4a7f7021b2ae40d777a60497c6e2c5a185a68b04
UnmappedStack <jake.steinburger0@gmail.com>
2025-06-09 17:31:39 +1000

    [pedal] JSToken structures

--- src/jsengine.c
+++ src/jsengine.c
@@ -1,5 +1,23 @@
+// Pedal: the ECMAScript bytecode compiler + interpreter for Bikeshed
 #include <stdio.h>
 #include <miscutils.h>
+#include <stddef.h>
+#include <darray.h>
+
+typedef struct JSToken JSToken;
+typedef struct {
+    JSToken** items;
+    size_t len, cap;
+} JSTokens;
+
+typedef enum {
+    JSInteger, JSPlus, JSMinus, JSMul, JSDiv,
+} JSTokType;
+
+struct JSToken {
+    JSTokType ttype;
+    size_t val; // what this should be casted to depends on `ttype`
+};
 
 int run_js(char* content) {
     (void) content;
