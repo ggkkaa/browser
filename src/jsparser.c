16e3ab491536890611928a788062a5bd84b69655
UnmappedStack <jake.steinburger0@gmail.com>
2025-06-10 07:29:47 +1000

    [jsengine] fix dump_ast

--- src/jsparser.c
+++ src/jsparser.c
@@ -1,4 +1,5 @@
 #include <jsengine.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,13 +12,22 @@ int operation_precedence[] = {
     ['-'] = 1,
 };
 
+#define PRINT_SPACES(x) \
+    do { \
+        for (int si = 0; si < x; si++) printf(" "); \
+    } while (0)
 void dump_ast(ASTBranch *ast, int depth) {
     switch (ast->type) {
     case AST_NODE_UNSIGNED_INT:
-        printf("%.*s%zu\n", depth, " ", ast->UnsignedInteger.val);
+        PRINT_SPACES(depth); printf("-> %zu\n", ast->UnsignedInteger.val);
+        break;
+    case AST_NODE_BINOP:
+        PRINT_SPACES(depth); printf("-> BinOpNode: %c\n", ast->BinOpNode.op);
+        dump_ast(ast->BinOpNode.val1, depth + 4);
+        dump_ast(ast->BinOpNode.val2, depth + 4);
         break;
     default:
-        printf("%.*sInvalid branch\n", depth, " ");
+        PRINT_SPACES(depth); printf("-> Invalid node\n");
         break;
     };
 }
@@ -33,6 +43,7 @@ int gen_ast(JSTokens toks, ASTBranch *ast) {
         if (this_precedence >= min_tok_precedence) continue;
         min_tok_precedence = this_precedence;
         min_tok_idx = i;
+        printf("add %d (%c)\n", toks.items[i].ttype, toks.items[i].ttype);
     }
     if (min_tok_precedence == INIT_TOK_PREC) {
         if (toks.len != 1 || toks.items[0].ttype != JS_TOK_INTEGER) {
@@ -59,11 +70,11 @@ int gen_ast(JSTokens toks, ASTBranch *ast) {
         JSTokens toks_after  = toks;
         toks_before.len = min_tok_idx;
         toks_after.items += min_tok_idx + 1;
-        toks_after.len -= min_tok_idx;
+        toks_after.len -= min_tok_idx + 1;
         ast->BinOpNode.val1 = (ASTBranch*) malloc(sizeof(ASTBranch));
         ast->BinOpNode.val2 = (ASTBranch*) malloc(sizeof(ASTBranch));
-        gen_ast(toks_before, ast->BinOpNode.val1);
-        gen_ast(toks_after, ast->BinOpNode.val2);
+        if (gen_ast(toks_before, ast->BinOpNode.val1) < 0) return -1;
+        if (gen_ast(toks_after, ast->BinOpNode.val2) < 0) return -1;
         ast->BinOpNode.op = toks.items[min_tok_idx].ttype;
         ast->type = AST_NODE_BINOP;
     }
