fe8c44e65e94e6866d6c46b3994869fcc5af710a
DcraftBg <czl_obshta@abv.bg>
2025-06-27 16:42:20 +0300

    [css] Parsing font: + fixing up bugs

--- src/css/apply.c
+++ src/css/apply.c
@@ -13,7 +13,7 @@ void apply_css_styles(HTMLTag* tag, float rootFontSize) {
         if(strcmp(att->name->data, "display") == 0) {
             if(att->args.len > 1) fprintf(stderr, "WARN ignoring extra args to display\n");
             else if(att->args.len < 1) {
-                fprintf(stderr, "ERROR too few args to display!\n");
+                fprintf(stderr, "CSS:ERROR too few args to display!\n");
                 continue;
             }
             CSSArg* arg = &att->args.items[i];
@@ -24,10 +24,33 @@ void apply_css_styles(HTMLTag* tag, float rootFontSize) {
             } else if(arg->value_len == 12 && memcmp(arg->value, "inline-block", 12) == 0) {
                 tag->display = CSSDISPLAY_INLINE_BLOCK;
             }
+        } else if (strcmp(att->name->data, "font") == 0) {
+            switch(att->args.len) {
+            case 3: {
+                CSSArg font_size = att->args.items[0];
+                CSSArg font = att->args.items[1];
+                CSSArg family = att->args.items[2];
+                fprintf(stderr, "CSS:TODO "__FILE__":"STRINGIFY1(__LINE__)" font: %.*s, family: %.*s\n", (int)font.value_len, font.value, (int)family.value_len, family.value);
+                const char* css_start = font_size.value;
+                const char* css_end = font_size.value + font_size.value_len;
+                int e = css_compute_numeric(rootFontSize, css_start, css_end, &css_start, &tag->fontSize);
+                if(e < 0) {
+                    fprintf(stderr, "CSS:ERROR parsing font_size in font: %s\n", csserr_str(e));
+                    continue;
+                }
+                if(css_start < css_end && css_start[0] == '/') {
+                    fprintf(stderr, "CSS:TODO "__FILE__":"STRINGIFY1(__LINE__)" parse font-size/line height\n");
+                    continue;
+                }
+
+            } break;
+            default:
+                fprintf(stderr, "CSS:WARN Ignoring %zu number of arguments to font: property\n", att->args.len);
+            }
         } else if(strcmp(att->name->data, "font-size") == 0) {
             if(att->args.len > 1) fprintf(stderr, "WARN ignoring extra args to font-size\n");
             else if(att->args.len < 1) {
-                fprintf(stderr, "ERROR too few args in font-size!\n");
+                fprintf(stderr, "CSS:ERROR too few args in font-size!\n");
                 continue;
             }
             CSSArg* arg = &att->args.items[0];
@@ -38,7 +61,7 @@ void apply_css_styles(HTMLTag* tag, float rootFontSize) {
                 continue;
             }
         } else {
-            fprintf(stderr, "WARN "__FILE__":" STRINGIFY1(__LINE__)": Unhandled attribute: `%s`\n", att->name->data);
+            fprintf(stderr, "WARN "__FILE__":" STRINGIFY1(__LINE__)" Unhandled attribute: `%s`\n", att->name->data);
         }
     }
     for(size_t i = 0; i < tag->children.len; ++i) {
