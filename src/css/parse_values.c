ee971a5aa8105b470e5c2582d83a4b8089e48877
UnmappedStack <jake.steinburger0@gmail.com>
2025-06-28 15:32:37 +1000

    [css] calculate string lengths at compile time for colours

--- src/css/parse_values.c
+++ src/css/parse_values.c
@@ -55,6 +55,7 @@ uint8_t css_parse_decimal_byte(const char* css_content, const char* css_content_
 static uint8_t xdigit2value(int digit) {
     return digit <= '9' ? digit - '0' : (tolower(digit) - 'a' + 10);
 }
+#define STRLEN(x) (sizeof(x) - 1)
 int css_compute_color(const char* css_content, const char* css_content_end, const char** end, CSSColor* result) {
     const char* word = css_content;
     while(css_content < css_content_end && isalnum(css_content[0])) css_content++;
@@ -88,27 +89,27 @@ int css_compute_color(const char* css_content, const char* css_content_end, cons
         static struct {
             const char* name;
             CSSColor color;
+            size_t len;
         } named_colors[] = {
-            { "black"  , 0x000000FF },
-            { "silver" , 0xC0C0C0FF },
-            { "gray"   , 0x808080FF },
-            { "white"  , 0xFFFFFFFF },
-            { "maroon" , 0x800000FF },
-            { "red"    , 0xFF0000FF },
-            { "purple" , 0x800080FF },
-            { "fuchsia", 0xFF00FFFF },
-            { "green"  , 0x008000FF },
-            { "lime"   , 0x00FF00FF },
-            { "olive"  , 0x808000FF },
-            { "yellow" , 0xFFFF00FF },
-            { "navy"   , 0x000080FF },
-            { "blue"   , 0x0000FFFF },
-            { "teal"   , 0x008080FF },
-            { "aqua"   , 0x00FFFFFF },
+            { "black"  , 0x000000FF, STRLEN("black"  ) },
+            { "silver" , 0xC0C0C0FF, STRLEN("silver" ) },
+            { "gray"   , 0x808080FF, STRLEN("gray"   ) },
+            { "white"  , 0xFFFFFFFF, STRLEN("white"  ) },
+            { "maroon" , 0x800000FF, STRLEN("maroon" ) },
+            { "red"    , 0xFF0000FF, STRLEN("red"    ) },
+            { "purple" , 0x800080FF, STRLEN("purple" ) },
+            { "fuchsia", 0xFF00FFFF, STRLEN("fuchsia") },
+            { "green"  , 0x008000FF, STRLEN("green"  ) },
+            { "lime"   , 0x00FF00FF, STRLEN("lime"   ) },
+            { "olive"  , 0x808000FF, STRLEN("olive"  ) },
+            { "yellow" , 0xFFFF00FF, STRLEN("yellow" ) },
+            { "navy"   , 0x000080FF, STRLEN("navy"   ) },
+            { "blue"   , 0x0000FFFF, STRLEN("blue"   ) },
+            { "teal"   , 0x008080FF, STRLEN("teal"   ) },
+            { "aqua"   , 0x00FFFFFF, STRLEN("aqua"   ) },
         };
         for(size_t i = 0; i < sizeof(named_colors)/sizeof(*named_colors); ++i) {
-            // TODO: precompute strlen(named_colors[i])
-            if(word_len == strlen(named_colors[i].name) && memcmp(word, named_colors[i].name, word_len) == 0) {
+            if(word_len == named_colors[i].len && memcmp(word, named_colors[i].name, word_len) == 0) {
                 value = named_colors[i].color;
                 goto end;
             }
